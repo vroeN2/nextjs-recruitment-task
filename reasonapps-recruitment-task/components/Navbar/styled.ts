3d21f45c982c61ccc269039b96a3b4520a9d6bdc
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2022-10-27 21:17:16 +0200

    feat: add mobile view
    * create rules for mobile view
    * cleanup code
    * remove redundant code

--- reasonapps-recruitment-task/components/Navbar/styled.ts
+++ reasonapps-recruitment-task/components/Navbar/styled.ts
@@ -4,13 +4,11 @@ export const NavbarWrapper = styled.div`
   z-index: 3;
   width: 100vw;
   height: 10vh;
-  /* background: ${(props) => props.color}; */
+  min-height: 100px;
   background: #0f0f0f;
   position: fixed;
   top: 0;
   left: 0;
-  /* color: ${(props) =>
-    props.color === "#ffffff" ? "#1d1d1d" : "#ffffff"}; */
   color: white;
   transition: 0.3s all ease;
   box-sizing: border-box;
@@ -19,6 +17,12 @@ export const NavbarWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  @media only screen and (max-width: 600px) {
+    top: auto;
+    bottom: 0;
+    left: 0;
+  }
 `;
 
 export const LogoWrapper = styled.div`
@@ -27,6 +31,14 @@ export const LogoWrapper = styled.div`
   align-items: center;
   font-size: 4rem;
   pointer-events: none;
+
+  @media only screen and (max-width: 820px) {
+    font-size: 2.5rem;
+  }
+
+  @media only screen and (max-width: 600px) {
+    display: none;
+  }
 `;
 
 export const LinksWrapper = styled.div`
@@ -34,12 +46,28 @@ export const LinksWrapper = styled.div`
   justify-content: space-evenly;
   align-items: center;
   width: 50%;
+
+  @media only screen and (max-width: 600px) {
+    width: 100%;
+  }
 `;
 
 export const MenuItemWrapper = styled.div`
-  /* color: ${(props) =>
-    props.color === "#ffffff" ? "#1d1d1d" : "#ffffff"}; */
   color: white;
   transform: scale(1.3);
   cursor: pointer;
+
+  & > strong {
+    @media only screen and (max-width: 820px) {
+      display: none;
+    }
+  }
+
+  & > .menu__icon {
+    display: none;
+
+    @media only screen and (max-width: 820px) {
+      display: block;
+    }
+  }
 `;
