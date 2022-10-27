20fb8962c48db9a658de9fcb33ba99f63d53dc3e
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2022-10-27 13:09:16 +0200

    fix: legacy implementation of redux provider

--- reasonapps-recruitment-task/pages/_app.tsx
+++ reasonapps-recruitment-task/pages/_app.tsx
@@ -2,13 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import { wrapper } from "../store/store";
+import { Provider } from "react-redux";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { store } = wrapper.useWrappedStore(pageProps);
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
 
-export default wrapper.withRedux(MyApp);
+export default MyApp;
